14c08f9321d9af99fafb8d779b19d904d1c39df6
Josef Adlao <jadlao@gmail.com>
2017-08-20 11:46:37 +1000

    restyled media div

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
     var searchForm = $('#searchSong');
     var searchValue = $('#searchSong :input')[0];
         
-    searchForm.submit(function(){
-        event.preventDefault()
+    searchForm.submit(function(e){
+        e.preventDefault()
         callItunesApi(searchValue.value);
     });
 });
@@ -27,8 +27,8 @@ function callItunesApi(searchTerm){
             console.log(data);
             var songListContainer = $('.songContainer');
             // empty the array each time you submit a search
-            songListContainer.empty()
-            prepareSongList(data.results)
+            songListContainer.empty();
+            prepareSongList(data.results);
             // .results refers to name of group of arrays
         })
         .fail(function(err){
@@ -54,32 +54,34 @@ function prepareSongList(songList){
 function songElement(song){
     //console.log(song); // returns individual data objects
     
-    var songElement = $('<div class="media"><div class="media-left"></div><div class="media-body"></div></div>');
+    var songElement = $('<div class="media text-center"><div class="media-left"></div><div class="media-body"></div><div class="media-player"></div></div>');
     
-    var trackName = $('<h2></h2>');
+    var trackName = $('<h3></h3>');
     var collectionName = $('<p></p>');
     var artistName = $('<p></p>');
     var artworkUrl100 = $('<img src="" />');
-    var artistViewUrl = $('<a href="">Link</a>');
-    var previewUrl = $('<br><br><audio controls><source src="" type="audio/m4a"></audio>')
+    var artistViewUrl = $('<a href="">More about the artist</a>');
+    var previewUrl = $('<br><audio controls><source src="" type="audio/m4a"></audio>')
     
-    trackName.text('Song Name: ' + song.trackName);
+    trackName.text('Song: ' + song.trackName);
     collectionName.text('Album: ' + song.collectionName);
     artistName.text('Artist: ' + song.artistName);
     artworkUrl100.attr('src', song.artworkUrl100);
     artistViewUrl.attr('href', song.artistViewUrl);
     previewUrl.attr('src', song.previewUrl);
-    
+        
     songElement
         .find('.media-body')
             .append(trackName)
             .append(collectionName)
             .append(artistName)
             .append(artistViewUrl)
-            .append(previewUrl)
     songElement
         .find('.media-left')
             .append(artworkUrl100)
+    songElement 
+        .find('.media-player')
+            .append(previewUrl)
     
     return songElement
-}
+};
